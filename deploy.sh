f4d4040dc9c5c5cc20f7bcf4dda3296e9ef5a6e9
m.aalemi <m.aalemi@rahkar.co>
2019-03-24 12:57:23 +0430

    report-new

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Cleaning and building app ..."
-./gradlew clean installDist
+gradle clean installDist
 
 echo "Building docker ..."
-docker build -t wallet-server:1.0 --no-cache .
+sudo docker build -t wallet-server:1.0 --no-cache .
 
 echo "Running docker ..."
-docker run --network="host" wallet-server:1.0
+sudo docker run --network="host" wallet-server:1.0
